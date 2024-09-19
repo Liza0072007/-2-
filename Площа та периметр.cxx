41c411f4c6c828b7f1a08d2beb4f42ea34085540
Liza0072007 <verbickaaliza11@gmail.com>
2024-09-19 18:37:49 +0300

    Add files via upload

---
+++ Площа та периметр.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    double a, b, S, P;
+    
+    b = 7;
+    a = 5;
+  S = a * b;
+  P = 2 * (a + b);
+  
+  cout << " Периметр: " << P << endl;
+  
+ cout << "Площа " << S << endl;
+
+
+    return 0;
+}

24e594a2cf2eee397800181539ef3115d724a23f
Liza0072007 <verbickaaliza11@gmail.com>
2024-09-19 18:27:03 +0300

    Update переведення температури.cxx

--- переведення температури.cxx
+++ переведення температури.cxx
@@ -1,15 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-    double TF, TC;
-
-    cout << "Введите температуру в градусах Фаренгейта: ";
-    cin >> TF;
-
-   TC = (TF - 32) * 5.0 / 9.0;
-
-    cout << "Температура в градусах Цельсия: " << TC << endl;
-
-    return 0;
-}
